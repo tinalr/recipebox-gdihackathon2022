44f410c9091be8b1cc0f4a507ae0311e499a7c19
sasszz <luchevreuil@gmail.com>
2022-11-16 15:44:06 -0800

    Init Commit, API call rough draft

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ApiCall from './components/ApiCall'
+
+function App() {
+  return (
+    <div className="App">
+      <ApiCall />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ApiCall.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const ApiCall = props => {
+    //Note the second argument is an empty array.
+    const [responseData, setResponseData] = useState([]);
+    useEffect(()=>{
+        axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=a5c113f14a144ce89576d7bcbc7f3dca')
+            .then(response=>{setResponseData(response.results)})
+    }, []); 
+    return(
+        <div>
+          <p>Outside Response</p>
+          {responseData &&
+            responseData.map((responseData, idx) => {
+            return <div key={idx}>
+              <p>Inside Response</p>
+              <p>{responseData.title}</p>
+            </div>
+          })}
+        </div>
+    )
+}
+
+export default ApiCall
