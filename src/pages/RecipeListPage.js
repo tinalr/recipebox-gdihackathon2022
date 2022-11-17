030505839d4c5d99f9fc8f9afeaacb26eb02a229
sasszz <luchevreuil@gmail.com>
2022-11-16 19:59:38 -0800

    small edit

--- src/pages/RecipeListPage.js
+++ src/pages/RecipeListPage.js
@@ -4,25 +4,24 @@ import axios from 'axios';
 const RecipeListPage = () => {
 
   const [responseData, setResponseData] = useState([]);
-  const [responseDataDescription, setResponseDataDescription] = useState([]);
 
   const foodDiet = "Vegan"
   // vegan, vegetarian, ketogenic
   // &diet=${foodDiet}
   // https://spoonacular.com/food-api/docs#Diets
 
-  const foodIntolerance = ""
+  const foodIntolerances = ""
   // peanut, dairy, seafood, soy, wheat
-  // &intolerance=${foodIntolerance}
+  // &intolerances=${foodIntolerances}
   // https://spoonacular.com/food-api/docs#Intolerances
-  // for each intolerance selected, send to this page "&intolerance=*intoleranceselected*"
+  // for each intolerance selected, send to this page "&intolerances=*intoleranceselected*"
   // concatenate all selections together into one variable
   // user can select 1 diet and as many intolerances as are available as buttons on previous page
 
   // API call retrieves 10 recipes with diet/intolerance selections
   // Only provides Image and Title and ID
   useEffect(() => {
-    axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=a5c113f14a144ce89576d7bcbc7f3dca&diet=${foodDiet}&intolerance=${foodIntolerance}`)
+    axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=a5c113f14a144ce89576d7bcbc7f3dca&diet=${foodDiet}&intolerances=${foodIntolerance}`)
       .then(response => { setResponseData(response.data.results) })
   }, []);
 
