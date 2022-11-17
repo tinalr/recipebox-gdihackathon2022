09bb6f681e953073319a09e9d95e7047b20c87e6
sasszz <luchevreuil@gmail.com>
2022-11-16 19:10:46 -0800

    API Call for RecipeListPage

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const ApiCall = props => {
+const ApiCall = () => {
     //Note the second argument is an empty array.
     const [responseData, setResponseData] = useState([]);
     useEffect(()=>{
         axios.get('https://api.spoonacular.com/recipes/complexSearch?apiKey=a5c113f14a144ce89576d7bcbc7f3dca')
             .then(response=>{setResponseData(response.results)})
     }, []); 
+
     return(
         <div>
           <p>Outside Response</p>
--- src/pages/RecipeListPage.js
+++ src/pages/RecipeListPage.js
@@ -1,7 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const RecipeListPage = () => {
+
+  const [responseData, setResponseData] = useState([]);
+
+  const foodDiet = "vegan"
+  // vegan, vegetarian, Gluten Free
+  // &diet=${foodDiet}
+  // https://spoonacular.com/food-api/docs#Diets
+
+  const foodIntolerance = 
+  // peanut, dairy, seafood, soy
+  // &intolerances=${foodIntolerance}
+  // https://spoonacular.com/food-api/docs#Intolerances
+
+  useEffect(() => {
+    axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=a5c113f14a144ce89576d7bcbc7f3dca&query=${foodDiet}`)
+      .then(response => { setResponseData(response.data.results) })
+  }, []);
+
   return (
-    <div>Recipes</div>
+    <div>
+      <p>Outside Response</p>
+      {responseData &&
+        responseData.map((responseData, idx) => {
+          return <div key={idx}>
+            <p>Inside Response</p>
+            <p>{responseData.title}</p>
+          </div>
+        })}
+    </div>
   );
 };
 
