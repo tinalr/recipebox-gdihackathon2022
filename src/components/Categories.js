f6bc7039300717f3c9de602364664fa2049d36e1
ksuares1 <kimberlyjsuares@gmail.com>
2022-11-17 15:25:12 -0500

    added header to landing page

---
+++ src/components/Categories.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import {
+    Container, 
+    Col, 
+    Row
+} from 'reactstrap';
+
+<>
+<Container>
+  <h6>
+    xs=“2“
+  </h6>
+  <Row xs="2">
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+  </Row>
+  <h6>
+    xs=“3“
+  </h6>
+  <Row xs="3">
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+  </Row>
+  <h6>
+    xs=“4“
+  </h6>
+  <Row xs="4">
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+  </Row>
+  <h6>
+    xs=“4“
+  </h6>
+  <Row xs="4">
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col
+      className="bg-light border"
+      xs="6"
+    >
+      xs=“6“
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+  </Row>
+  <h6>
+    xs=“1“ sm=“2“ md=“4“
+  </h6>
+  <Row
+    md="4"
+    sm="2"
+    xs="1"
+  >
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+    <Col className="bg-light border">
+      Column
+    </Col>
+  </Row>
+</Container>
+
+
+</>
