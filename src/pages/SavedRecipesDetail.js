cb8eee61fb2dac4103f88e10d0993adc7ed957b2
Christina Rontell <clrontell@gmail.com>
2022-11-17 19:24:41 -0600

    saved recipes detail card

--- src/pages/SavedRecipesDetail.js
+++ src/pages/SavedRecipesDetail.js
@@ -1,11 +1,13 @@
-import { Container, Card, CardHeader, CardTitle, CardText, CardBody, CardFooter } from "reactstrap";
+import { Col, Row, Container, Card, CardHeader, CardTitle, CardText, CardBody, CardFooter } from "reactstrap";
 
 const SavedRecipesDetail = () => {
   return (
-    <Container >
-      <Card>
+    <>
+
+      <Card style={{ backgroundColor: 'lightblue', width: '50%', }}>
         <CardHeader tag='h1'>RecipeName</CardHeader>
         <img src="chrome://branding/content/about-logo@2x.png" alt="" />
+
         <CardBody>
           <CardTitle tag='h2'>Ingredients</CardTitle>
           <CardText>
@@ -15,13 +17,15 @@ const SavedRecipesDetail = () => {
           <CardText>
             Steps List
           </CardText>
+          <CardTitle tag='h2'>Notes</CardTitle>
+          <CardText>
+            Recipe Notes
+          </CardText>
         </CardBody>
-        
-        <CardFooter>
-          Notes
-        </CardFooter>
+
       </Card>
-    </Container>
+
+    </>
   );
 };
 export default SavedRecipesDetail;
