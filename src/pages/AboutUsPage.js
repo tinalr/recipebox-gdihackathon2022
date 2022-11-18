655e23545ffdb658579eeb8fe66044389b0ffef0
sasszz <luchevreuil@gmail.com>
2022-11-17 23:47:06 -0800

    Formatting

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -25,16 +25,35 @@ import {
 const AboutUsPage = () => {
   return (
     <Container>
-      <Card className="mb-3 p-3">
-        <CardTitle tag="h5" className="text-center">
-          About Us
-        </CardTitle>
-        <p>GirlDevelopIt (GDI) is a an organization with a mission to create welcoming, supportive opportunities for women and non-binary adults to learn software development skills. On Wednesday, November 16, 2022, GDI kicked off their 2nd Annual Virtual Hackathon with a theme of "Hack for Health". Developers from all over the world came together to pitch app ideas and form teams in order to present a finished product by Saturday, November 19, 2022.</p>
+      <Card className="mb-3 p-3"
+        style={{
+          width: '60rem'
+        }}
+      >
+        <Row>
+          <Col>
+          </Col>
+          <Col>
+            <p>Our team came together around a passion for nutrition and inclusion for diverse dietary needs during GirlDevelopIt's 2nd Annual Virtual Hackathon. Over the course of three days, we developed our application idea, coordinated teams for the frontend and backend, met in daily stand-ups, designed in Figma, worked asynchronously across timezones, thought through the user experience and built a close-knit community. All of us contributed to the success of this project, and we're pleased to present to you "Note to Chef"!</p>
+          </Col>
+        </Row>
+      </Card>
+      <Card className="mb-3 p-3"
+        style={{
+          width: '60rem'
+        }}
+      >
+        <Row>
+          <Col>
+            <p>"Note to Chef" makes adjusting for dietary accommodations simple by bringing common restrictions to the front page of the app. From the home page, select any number of overlapping dietary restrictions and a curated list of 10 top recipes will populate!</p>
+            <p>
+              If you find a recipe you enjoy, you can save it to your profile for future use. A notes feature  on saved recipes provides the ability to list adaptations to the recipe, track the success and struggles with each attempt, and rank the recipe difficulty. You can also store recipes to a collection when planning large family gatherings.
+              "Chef it Up" makes it easy to take into consideration the unique dietary needs of yourself, and your guests!</p>
+          </Col>
+          <Col>
+          </Col>
 
-        <p>Our team came together around a passion for nutrition and inclusion for diverse dietary needs. Over the course of three days, we developed our application idea, coordinated teams for the frontend and backend, met in daily stand-ups, designed in Figma, worked asynchronously across timezones, thought through the user experience and built a close-knit community. All of us contributed to the success of this project, and we're pleased to present to you "Note to Chef"!</p>
-        <p>"Note to Chef" makes adjusting for dietary accommodations simple by bringing common restrictions to the front page of the app. From the home page, select any number of overlapping dietary restrictions and a curated list of 10 top recipes will populate!
-          If you find a recipe you enjoy, you can save it to your profile for future use. A notes feature  on saved recipes provides the ability to list adaptations to the recipe, track the success and struggles with each attempt, and rank the recipe difficulty. You can also store recipes to a collection when planning large family gatherings.
-          "Chef it Up" makes it easy to take into consideration the unique dietary needs of yourself, and your guests!</p>
+        </Row>
       </Card>
       <Row>
         <Col className="mb-3"><Christine /></Col>
