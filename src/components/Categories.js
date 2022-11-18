ec6f86b913915de38ed67ca6104b3f8cb4893159
ksuares1 <kimberlyjsuares@gmail.com>
2022-11-17 21:30:18 -0500

    Added CSS to add spacing for columns

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,138 +1,85 @@
+import { Container, Col, Row, CardTitle, CardBody } from "reactstrap";
 
-import {
-    Container, 
-    Col, 
-    Row, 
-     CardTitle, 
-     CardBody,
-} from 'reactstrap';
-
-const Categories =() =>{
-    return(
+const Categories = () => {
+  return (
     <>
-    <div>
+      <div class="catergories-header">
         <h1>Select a Category</h1>
-    </div>
-<Container>
-  
-  {/* Card 1  */}
-  <Row xs="2">
-     <Col className=" border">
-     <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
- 
-    {/*Card 2   */}
-    <Col className="border">
-    <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
-  </Row>
- 
-    {/*Card 3   */}
+      </div>
+      <Container class="cards-container">
+        {/* Card 1  */}
+        <Row xs="2">
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
 
-    <Row xs="2">
-     <Col className=" border">
-     <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
- 
-    {/*Card 4   */}
-    <Col className="border">
-    <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
-  </Row>
- 
+          {/*Card 2   */}
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
+        </Row>
+
+        {/*Card 3   */}
+
+        <Row xs="2">
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
+
+          {/*Card 4   */}
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
+        </Row>
+
         {/* Card 5  */}
         <Row xs="2">
-     <Col className=" border">
-     <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
- 
-    {/*Card 6  */}
-    <Col className="border">
-    <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
-  </Row>
-  <Row xs="2">
-     <Col className=" border">
-     <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
- 
-    {/*Card 7  */}
-    <Col className="border">
-    <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-      </CardBody>
-    </Col>
-  </Row>
- 
- 
-</Container>
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
 
+          {/*Card 6  */}
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
+        </Row>
+        <Row xs="2">
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
 
-</>
- 
- );
+          {/*Card 7  */}
+          <Col className="categories-col">
+            <img alt="Card" src="https://picsum.photos/300/200" />
+            <CardBody>
+              <CardTitle tag="h5">Card Title</CardTitle>
+            </CardBody>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
-export default Categories; 
+export default Categories;
