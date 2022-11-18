e8211f5a3ff211af9be0639edd3fd47c3ce80ec2
sasszz <luchevreuil@gmail.com>
2022-11-17 16:22:18 -0800

    Imgs, Icons and About Page

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,8 +1,75 @@
+import Github from '../assets/icons/github.png'
+import LinkedIn from '../assets/icons/linkedin.png'
+import Lucie from '../assets/profilepics/LucieChevreuil.png'
+import Christina from '../assets/profilepics/ChristinaRontell.jpg'
 
 const AboutUsPage = () => {
   return (
-    <div>About Us</div>
+    <div>
+      <h1>About Us</h1>
+    </div>
+
   );
 };
 
 export default AboutUsPage;
+
+<div>
+{/* Hackathon Information */}
+<p>GirlDevelopIt (GDI) is a an organization with a mission to create welcoming, supportive opportunities for women and non-binary adults to learn software development skills. On Wednesday, November 16, 2022, GDI kicked off their 2nd Annual Virtual Hackathon with a theme of "Hack for Health". Developers from all over the world came together to pitch app ideas and form teams in order to present a finished product by Saturday, November 19, 2022.</p>
+<p>Our team was inspired </p>
+
+{/* Profile Information */}
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Christine Boegemann</p>
+<p>Role: Backend Developer</p>
+<a href="https://github.com/cboegemann"><img src={Github} alt="Github" /></a>
+<a href=""><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={Lucie} alt="Profile Picture"/>
+<p>Lucie Chevreuil</p>
+<p>Role: Fullstack Developer</p>
+<a href="https://github.com/sasszz?tab=repositories"><img src={Github} alt="Github" /></a>
+<a href="https://www.linkedin.com/in/lucie-c-sasszz/"><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Polina Hermans</p>
+<p>Role: Frontend Developer</p>
+<a href="https://github.com/ph202"><img src={Github} alt="Github" /></a>
+<a href="https://www.linkedin.com/in/polina-hermans-2b9838a4/"><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Lindsay Rogers</p>
+<a href=""><img src={Github} alt="Github" /></a>
+<a href=""><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={Christina} alt="Profile Picture"/>
+<p>Christina Rontell</p>
+<p>Role: Frontend Developer</p>
+<a href="https://github.com/tinalr"><img src={Github} alt="Github" /></a>
+<a href="https://www.linkedin.com/in/christina-rontell/"><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Kimberly Suares</p>
+<p>Role: Frontend Developer</p>
+<a href="https://github.com/ksuares1"><img src={Github} alt="Github" /></a>
+<a href="https://www.linkedin.com/in/kimberlysuares/"><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Gauri Tilloo</p>
+<p>Role: Project Manager</p>
+<a href="www.linkedin.com/in/gtilloo"><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Mollie Whaley</p>
+<p>Role: Frontend Developer</p>
+<a href="https://github.com/molliewhaley"><img src={Github} alt="Github" /></a>
+<a href=""><img src={LinkedIn} alt="LinkedIn" /></a>
+
+<img src={PlaceHolder} alt="Profile Picture"/>
+<p>Carlisha Wilson</p>
+<p>Role: UX/UI, Design</p>
+<a href=""><img src={Github} alt="Github" /></a>
+<a href=""><img src={LinkedIn} alt="LinkedIn" /></a>
+
+</div>
